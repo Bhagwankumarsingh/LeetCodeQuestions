5445c93db0dd8c9b8eef3151b0c2dc69d13aecbc
Bhagwan Kumar Singh <117030732+Bhagwankumarsingh@users.noreply.github.com>
2023-09-04 16:18:49 +0530

    Time: 3 ms (33.02%), Space: 6.6 MB (25.17%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int solve(int m,int n,vector<vector<int>>&dp){
+        if(m==0 && n==0) return 1;
+
+        if(m<0 || n<0) return 0;
+        if(dp[m][n]!=-1) return dp[m][n];
+        int goUp=solve(m-1,n,dp);
+        int goLeft=solve(m,n-1,dp);
+        return dp[m][n]=goUp+goLeft;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,dp);
+    }
+};
