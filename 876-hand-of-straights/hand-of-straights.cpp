7d05cad754bb9da665f77421dc99de64a7b651d4
Bhagwan Kumar Singh <117030732+Bhagwankumarsingh@users.noreply.github.com>
2024-05-02 21:02:03 +0530

    Time: 60 ms (59.28%) | Memory: 31.9 MB (20.67%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        if(hand.size()%groupSize!=0) return false;
-
-        unordered_map<int,int>count;
+        if(hand.size()%groupSize!=0){
+            return false;
+        }
+        unordered_map<int,int>mp;
         for(auto it:hand){
-            count[it]++;
+            mp[it]++;
         }
         priority_queue<int,vector<int>,greater<int>>pq;
-        for(auto i:count){
+        for(auto i:mp){
             pq.push(i.first);
         }
         while(!pq.empty()){
-            int start = pq.top();
-            // Attempt to form a group starting from the smallest element in pq
-            for (int i = 0; i < groupSize; i++) {
-                int card = start + i;
-                if (count[card] == 0) {
-                    return false; // If there's any card missing in the sequence, fail
+            int t=pq.top();
+            for(int i=0;i<groupSize;i++){
+                int s=t+i;
+                if(mp[s]==0){
+                    return false;
                 }
-                
-                count[card]--;
-                if (count[card] == 0) {
-                    pq.pop(); // Only pop when we use up a card count to avoid messing up the loop
-                    // if (card != pq.top()) return false; // Check sync between count and priority queue
+                mp[s]--;
+                if(mp[s]==0){
+                    pq.pop();
                 }
             }
-
         }
         return true;
     }
