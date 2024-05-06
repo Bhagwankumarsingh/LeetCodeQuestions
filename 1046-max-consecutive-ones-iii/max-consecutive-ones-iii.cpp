f67085f511a3dc6a4db3e95ffa89d513bc751628
Bhagwan Kumar Singh <117030732+Bhagwankumarsingh@users.noreply.github.com>
2024-05-06 11:30:14 +0530

    Time: 51 ms (12.26%) | Memory: 57.9 MB (12.37%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int n=nums.size();
-        int zeroCnt=0;
-        int j=0;
         int i=0;
-        int ans=0;
+        int j=0;
+        int n=nums.size();
+        int cntZ=0;
+        int ans=INT_MIN;
         while(j<n){
-            if(nums[j]==0){
-                zeroCnt++;
+           if(nums[j]==0){
+            cntZ++;
+           }
+           while(cntZ>k){
+            if(nums[i]==0){
+                cntZ--;
             }
-            
-                while(zeroCnt>k){
-                if(nums[i]==0){
-                    zeroCnt--;
-                }
-                i++;
-                }
-        ans=max(ans,j-i+1);
-        j++;
+            i++;
+           }
+           ans=max(ans,j-i+1);
+           j++;
         }
+        
         return ans;
     }
 };
