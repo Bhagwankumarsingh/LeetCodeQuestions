56fe8ccd39f8202dc8fd3ada24dbc4bc3aea00cc
Bhagwan Kumar Singh <117030732+Bhagwankumarsingh@users.noreply.github.com>
2024-05-06 11:02:07 +0530

    Time: 8 ms (82.67%) | Memory: 19 MB (59.95%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        int maxReach=0;
+        int currReach=0;
+        int jumps=0;
+        for(int i=0;i<n-1;i++){
+            maxReach=max(maxReach,nums[i]+i);
+            if(i==currReach){
+                jumps++;
+                currReach=maxReach;
+            }
+        }
+        return jumps;
+    }
+};
